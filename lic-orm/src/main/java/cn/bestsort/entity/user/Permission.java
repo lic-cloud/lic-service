c064db788b81784baa2673cb8af40d2ee96c2311
bestsort <bestsort126@gmail.com>
2020-09-07 09:47:26 +0800

    fix

--- lic-orm/src/main/java/cn/bestsort/entity/user/Permission.java
+++ lic-orm/src/main/java/cn/bestsort/entity/user/Permission.java
@@ -2,15 +2,11 @@ package cn.bestsort.entity.user;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 
 import cn.bestsort.entity.BaseEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.io.Serializable;
 
 /**
  * @author GoodTime0313
@@ -21,8 +17,7 @@ import java.io.Serializable;
 @Entity
 @ToString
 @EqualsAndHashCode(callSuper = true)
-public class Permission extends BaseEntity{
-
+public class Permission extends BaseEntity {
     /**
      * 菜单名称
      */
