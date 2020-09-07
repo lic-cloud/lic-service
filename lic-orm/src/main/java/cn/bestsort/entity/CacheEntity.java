81c963815b64d52c8cfae258942eb7f57056a3f5
bestsort <bestsort126@gmail.com>
2020-09-07 10:19:21 +0800

    add cache type

---
+++ lic-orm/src/main/java/cn/bestsort/entity/CacheEntity.java
@@ -0,0 +1,29 @@
+package cn.bestsort.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import cn.bestsort.enums.CacheType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author bestsort
+ * @version 1.0
+ * @date 2020-09-07 10:17
+ */
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class CacheEntity extends BaseEntity {
+
+
+    @Column
+    String key;
+
+    @Column
+    String value;
+
+    @Column
+    CacheType type;
+}
---
+++ lic-orm/src/main/java/cn/bestsort/enums/CacheType.java
@@ -0,0 +1,11 @@
+package cn.bestsort.enums;
+
+/**
+ * @author bestsort
+ * @version 1.0
+ * @date 2020-09-07 10:18
+ */
+public enum CacheType {
+    META_DATA,
+    CACHE;
+}
