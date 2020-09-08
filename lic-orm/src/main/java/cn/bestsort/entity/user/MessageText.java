8a58e4aab48424dda6c0607f63126dd705b5550a
lulinjun1111 <1360002059@qq.com>
2020-09-08 09:08:02 +0800

    add entity

---
+++ lic-orm/src/main/java/cn/bestsort/entity/user/MessageText.java
@@ -0,0 +1,26 @@
+package cn.bestsort.entity.user;
+
+import cn.bestsort.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class MessageText extends BaseEntity {
+
+    @Column(nullable = false,length = 50)
+    private String title;
+
+    @Column(nullable = false,length = 300)
+    private String message;
+
+    @Column(nullable = false)
+    private Timestamp sendtime;
+}
