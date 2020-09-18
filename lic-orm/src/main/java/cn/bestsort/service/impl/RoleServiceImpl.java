9a3ace2b316d24e5ff9c73d740a9ea069a5042e5
bestsort <bestsort126@gmail.com>
2020-09-18 19:58:01 +0800

    fix style

--- lic-orm/src/main/java/cn/bestsort/service/impl/RoleServiceImpl.java
+++ lic-orm/src/main/java/cn/bestsort/service/impl/RoleServiceImpl.java
@@ -11,7 +11,6 @@ import cn.bestsort.service.AbstractBaseService;
 import cn.bestsort.service.RolePermissionService;
 import cn.bestsort.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
--- lic-starter/src/main/java/cn/bestsort/controller/NoticeController.java
+++ lic-starter/src/main/java/cn/bestsort/controller/NoticeController.java
@@ -19,7 +19,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
