78f544e8341374fecabfbe6be3cc09bd98ee3b7f
GoodTime0313 <1824461232@qq.com>
2020-09-26 10:57:41 +0800

    还原noticePubList页面代码。暂时发现问题：1.删除实现 2.页面内容排序 3.个人公告

--- lic-starter/src/main/java/cn/bestsort/controller/NoticeController.java
+++ lic-starter/src/main/java/cn/bestsort/controller/NoticeController.java
@@ -9,6 +9,7 @@ import cn.bestsort.model.entity.Notice.Status;
 import cn.bestsort.model.entity.NoticeRead;
 import cn.bestsort.model.entity.User;
 import cn.bestsort.model.vo.DataTable;
+import cn.bestsort.model.vo.NoticeReadVO;
 import cn.bestsort.model.vo.NoticeVO;
 import cn.bestsort.service.NoticeReadService;
 import cn.bestsort.service.NoticeService;
@@ -19,7 +20,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -125,5 +125,14 @@ public class NoticeController {
             .map(NoticeRead::getUserId)
             .collect(Collectors.toSet())
             .size();
+    }
+
+
+    @GetMapping("/published")
+    @ApiOperation(value = "公告列表")
+    public DataTable<NoticeReadVO> listNoticeReadVO(@RequestParam int draw,
+                                              @RequestParam int start,
+                                              @RequestParam int length) {
+        return null;
     }
 }
