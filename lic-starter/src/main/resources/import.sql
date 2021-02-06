d2b3558646be4fc52990204e15473b08d4213e57
GoodTime0313 <1824461232@qq.com>
2021-02-06 14:48:04 +0800

    fix

--- lic-starter/src/main/resources/import.sql
+++ lic-starter/src/main/resources/import.sql
@@ -26,6 +26,7 @@ INSERT INTO `permission` VALUES (38, NULL, NULL, '', '', '查询', 37, 'dict:que
 INSERT INTO `permission` VALUES (39, NULL, NULL, '', '', '新增', 37, 'dict:add', 100, 2);
 INSERT INTO `permission` VALUES (40, NULL, NULL, '', '', '删除', 37, 'dict:del', 100, 2);
 INSERT INTO `permission` VALUES (41, '2020-10-11 12:10:24', '2020-10-11 12:11:05', 'fa-file', 'pages/file/file-list.html	', '文件列表', 0, '', 100, 1);
+INSERT INTO `permission` VALUES (42, '2020-10-11 12:10:24', '2020-10-11 12:11:05', 'fa-file', 'pages/file/recycle.html	', '回收站', 0, '', 100, 1);
 
 -- ----------------------------
 -- Records of role
